d47726ae83aab352c8f34f9490900a85ee38c9eb
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2020-01-14 19:51:12 +0900

    Update reject.js

--- js/reject.js
+++ js/reject.js
@@ -1,6 +1,8 @@
 window.onload = function rejectIEEdge(){
 var userAgent = window.navigator.userAgent.toLowerCase();
 if ( userAgent.indexOf( "msie" ) !== -1 || userAgent.indexOf( "trident" ) !== -1 || userAgent.indexOf( "edge" ) !== -1 ){
+document.open();
 document.write("<h2>コンパス履歴書ジェネレーターは<br>Internet Explorer 及び Microsoft Edgeを<br>サポートしていません。<br>別のブラウザからアクセスしてください。</h2>");
+document.close();
 }
 }
