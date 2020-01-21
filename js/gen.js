b2a420f2df1efbb17ba064598bc8ee208506f5d3
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2020-01-22 00:41:15 +0900

    Update gen.js

--- js/gen.js
+++ js/gen.js
@@ -166,10 +166,10 @@ cpImg.src = img[7].src;
 ctx.fillStyle = "#000000";
 ctx.font = "30px 'monospace'";
 ctx.fillText("連絡先など", 30 , 930 );
-ctx.drawImage( twImg , 30 , 990 , 40 , 40 );
-ctx.drawImage( dcImg , 30 , 1050 , 40 , 40 );
-ctx.drawImage( spImg , 30 , 1110 , 40 , 40 );
-ctx.drawImage( cpImg , 30 , 1170 , 40 , 40 );
+ctx.drawImage( twImg , 30 , 940 , 50 , 50 );
+ctx.drawImage( dcImg , 30 , 1000 , 50 , 50 );
+ctx.drawImage( spImg , 30 , 1060 , 50 , 50 );
+ctx.drawImage( cpImg , 30 , 1120 , 50 , 50 );
 /*旧処理
 ctx.fillStyle = "#00bfff";
 ctx.font = "40px 'monospace'";
@@ -621,11 +621,11 @@ errorMsg += "フレンドコードには10桁の数字のみ入力できます(�
 ctx.font = "40px 'monospace'";
 //連絡先描画
 if ( tw != "@" ){
-ctx.fillText( tw , 350 , 990 );
+ctx.fillText( tw , 100 , 990 );
 }
-ctx.fillText( dc , 350 , 1050 );
-ctx.fillText( sp , 350 , 1110 );
-ctx.fillText( fc , 350 , 1170 );
+ctx.fillText( dc , 100 , 1050 );
+ctx.fillText( sp , 100 , 1110 );
+ctx.fillText( fc , 100 , 1170 );
 
 //日付
 ctx.font = "25px 'monospace'";
