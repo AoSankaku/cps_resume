238ec260e0ce33c27f2fe9b24cf40133dcb1be62
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2020-01-23 22:20:23 +0900

    Update gen.js

--- js/gen.js
+++ js/gen.js
@@ -7,10 +7,17 @@ var doingMaintainance = true;
 var titleColor = document.getElementById('titleColor');
 var maintainanceStart = "1月23日 17:00";
 var maintainanceEnd = "未定";
-var updateContents = "";
+var updateContents = "【アップデート内容】<br>"
+			+ "・JavaScript、Cookieの許可状態に応じたメッセージの表示<br>"
+			+ "・メンテナンス中のメッセージの表示<br>"
+			+ "・プロフィール画像のアップロード<br>";
 if ( doingMaintainance == true ){
 	console.log('メンテナンス中');
-	document.getElementById('maintainance').innerHTML = "<h3>現在メンテナンス中です<br>（" + maintainanceStart + "～" + maintainanceEnd + "）<br>" + updateContents + "<br>メンテナンス中に履歴書を作成すると<br>予期せぬ挙動が発生する可能性があります。<br>時間を置いてからアクセスしてください。<br></h3>";
+	document.getElementById('maintainance').innerHTML = 
+		'<div class="box-title">メンテナンス中</div><p>現在メンテナンス中です<br>（'
+		+ maintainanceStart + '～' + maintainanceEnd + '）<br>'
+		+ updateContents
+		+ '<br>メンテナンス中に履歴書を作成すると予期せぬ挙動が発生する可能性があります。時間を置いてからアクセスしてください。</p>';
 	titleColor.style.color = "#ff0000";
 	}
 	
