237e6141027f364ecc3de47b87ae0a30fa6cf0f4
Blue Triangle <58819229+AoSankaku@users.noreply.github.com>
2020-01-26 16:57:50 +0900

    Create loading.js

---
+++ js/loading.js
@@ -0,0 +1,9 @@
+//ローディング画面の処理
+window.onload = function loading(){
+
+	//ローディング画面のを先にする
+	let spinner = document.getElementById('my-spinner');
+	// .box に .loaded を追加してローディング表示を消す
+	spinner.classList.add('loaded');
+
+}
