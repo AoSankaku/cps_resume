c61f28fd34bd3a3a0978a706f9531e4ce4341947
Blue Triangle <58819229+AoSankaku@users.noreply.github.com>
2020-01-26 22:40:42 +0900

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,3 @@
 //ローディング画面の処理
-window.onload = function loading(){
-
-	//ローディング画面のを先にする
-	let formLoader = document.getElementById('formLoader');
-	// .box に .loaded を追加してローディング表示を消す
-	formLoader.classList.add('formLoader-complete');
-
-}
+//挫折しました許して
+let a = 0;
