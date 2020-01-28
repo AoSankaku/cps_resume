1ec1b29bab8168689d37eefcca74ef31835a2f87
Blue Triangle <58819229+AoSankaku@users.noreply.github.com>
2020-01-28 21:12:05 +0900

    Update gen.js

--- js/gen.js
+++ js/gen.js
@@ -652,7 +652,7 @@ ctx.font = "24px 'monospace'";
 //入力がない項目は省略するので関数を組み上に詰める
 var contactY = 945;
 const plus1 = 30;
-const plus2 = 18;
+const plus2 = 16;
 const cmFontSize = 24 + 2;
 
 //連絡先アイコン描画
