47bd5023496f238da46600bb8e4345313d5e275c
Blue Triangle <58819229+AoSankaku@users.noreply.github.com>
2020-11-01 21:08:12 +0900

    Update notices.js

--- js/notices.js
+++ js/notices.js
@@ -3,7 +3,7 @@
 //メンテ予定…"planned"	=>青色で表示（デフォルト）
 //メンテ中…"doing"		=>赤色で表示（CSSいじいじ）
 
-var doingMaintainance = "doing";
+var doingMaintainance = "none";
 
 var maintainanceStart = "11月1日 21:00";
 var maintainanceEnd = "11月1日 21:15";
